56bd4d9dd41efbab15c45762b28988e9e250d909
rmahajan466 <rmahajan466@gmail.com>
2024-06-22 13:06:27 +0530

    This is impact training practice

---
+++ Class_name.java
@@ -0,0 +1,12 @@
+class Class_name {
+    static int b = 20;
+    int a = 10;
+
+    public static void main(String[] args) {
+        int c = 30;
+        System.out.println(c);
+        System.out.println(b);
+        Class_name obj = new Class_name();
+        System.out.println(obj.a);
+    }
+}
---
+++ Example.java
@@ -0,0 +1,12 @@
+public class Example {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 40;
+        System.out.print(a);
+        System.out.print(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.printf("%d", a);
+        System.out.printf("%d", b);
+    }
+}
